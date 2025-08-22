f511ecd2d038d7ad5f778ae5fe8365f011ac7222
motatan79 <m.pirela@gmail.com>
2025-08-22 05:04:46 -0300

    Add cliente_create

--- project/cliente/forms.py
+++ project/cliente/forms.py
@@ -5,17 +5,17 @@ class ClienteForm(forms.ModelForm):
     class Meta:
         model = models.Cliente
         fields = "__all__" # Puedes especificar los campos que deseas incluir
-        # fields = ['nombre', 'apellido', 'email', 'telefono'] # Ejemplo de campos específicos
-        # Puedes personalizar los widgets y etiquetas de los campos si es necesario
+        #fields = ['nombre', 'apellido', 'email', 'telefono'] # Ejemplo de campos específicos
+        #Puedes personalizar los widgets y etiquetas de los campos si es necesario
         # widgets = {
         #     'nombre': forms.TextInput(attrs={'class': 'form-control'}),
         #     'apellido': forms.TextInput(attrs={'class': 'form-control'}),
         #     'email': forms.EmailInput(attrs={'class': 'form-control'}),
-        #     'telefono': forms.TextInput(attrs={'class': 'form-control'}),
+        #     # 'telefono': forms.TextInput(attrs={'class': 'form-control'}),
         # }
         # labels = {
         #     'nombre': 'Nombre',
         #     'apellido': 'Apellido',
         #     'email': 'Correo Electrónico',
-        #     'telefono': 'Teléfono',
+        #     # 'telefono': 'Teléfono',
         # }
