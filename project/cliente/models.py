7df69acfc2e7ab6fd88d0678fd0f188a88e8406b
motatan79 <m.pirela@gmail.com>
2025-09-29 11:27:24 -0300

    Cambiando contenido

--- project/cliente/models.py
+++ project/cliente/models.py
@@ -17,10 +17,11 @@ class Pais(models.Model):
 class Cliente(models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
-    nacimiento = models.DateField()
+    fecha_nacimiento = models.DateField()
     edad = models.IntegerField()
     email = models.EmailField(null=True, blank=True)
-    pais_origen_id = models.ForeignKey(Pais, on_delete=models.SET_NULL, null=True, blank=True, verbose_name="País de origen")
+    posicion1 = models.CharField(max_length=100, null=True, blank=True)
+    posicion2 = models.CharField(max_length=100, null=True, blank=True)
     
     def __str__(self):
         return f"{self.apellido}, {self.nombre}" if self.apellido else self.nombre
